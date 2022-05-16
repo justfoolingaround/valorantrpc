26dc6343481e992a1ead271834e91fe58ca3ef13
justfoolingaround <kr.justfoolingaround@gmail.com>
2022-05-16 17:11:25 +0545

    Fix: Time

--- valorantrpc/rpc/states/ingame/playing.py
+++ valorantrpc/rpc/states/ingame/playing.py
@@ -3,6 +3,7 @@ from ...utils import (
     get_gamemode_information,
     get_competitive_rank_information,
     parse_datetime,
+    get_map_information
 )
 
 from .. import idle
@@ -17,11 +18,12 @@ def set_presence(session, rpc_client, client_data):
 
     state, party_size = get_party_information(client_data)
     gamemode_asset, gamemode = get_gamemode_information(client_data)
+    gamemap, gamemap_asset = get_map_information(session, client_data)
 
     if client_data["queueId"] == "competitive":
         large_image, large_text = get_competitive_rank_information(session, client_data)
     else:
-        large_image, large_text = gamemode_asset, gamemode
+        large_image, large_text = gamemap_asset, gamemap
 
     ally_score, enemy_score = (
         client_data["partyOwnerMatchScoreAllyTeam"],
@@ -31,8 +33,8 @@ def set_presence(session, rpc_client, client_data):
     return rpc_client.update(
         state=state,
         details=f"{gamemode} // {ally_score} - {enemy_score}",
-        small_image="game_icon",
-        small_text="VALORANT",
+        small_image=gamemode_asset,
+        small_text=gamemode,
         large_image=large_image,
         large_text=large_text,
         party_size=party_size,
